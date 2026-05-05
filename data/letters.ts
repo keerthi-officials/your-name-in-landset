0f0959609b13c057cae15f15723be316ac22c8fa
keerthi-officials <keerthi.undurthi@gmail.com>
2026-05-05 13:16:32 +0530

    added 5 more letters

--- data/letters.ts
+++ data/letters.ts
@@ -1,109 +1,208 @@
-export const letterData: Record<string, { location: string; lat: string; lng: string, link?: string }[]> = {
-    a: [
-        { location: "Hickman, Kentucky", lat: "36°35'20.8 N", lng: "89°20'26.9 W", link: "https://www.google.com/maps/place/36%C2%B035'20.8%22N+89%C2%B020'26.9%22W/@36.5689533,-89.4212718,11.38z/data=!4m4!3m3!8m2!3d36.5891111!4d-89.3408056?entry=ttu" },
-        { location: "Farm Island, Maine", lat: "45°43'43.8 N ", lng: "69°46'08.9 W", link: "https://www.google.com/maps/place/45%C2%B043'45.4%22N+69%C2%B046'04.7%22W/@45.7292113,-69.7763592,6123m/data=!3m1!1e3!4m4!3m3!8m2!3d45.729275!4d-69.76798?entry=ttu" },
-        { location: "Lake Guakhmaz, Azerbaijan", lat: "40°39'50.8 N", lng: "47°06'36.2 E", link: "https://maps.app.goo.gl/tnrVaiMuFiGacH746" },
-        { location: "Yukon Delta, Alaska", lat: "62°33'17.7 N", lng: "164°56'10.3 W", link: "https://maps.app.goo.gl/AuARuTMTcT2ugLnq5" },
-        { location: "Lake Mjøsa, Norway", lat: "60°45'52.7 N", lng: "53°47'52.8 E", link: "https://www.google.com/maps/place/36%C2%B035'20.8%22N+89%C2%B020'26.9%22W/@36.5689533,-89.4212718,11.38z/data=!4m4!3m3!8m2!3d36.5891111!4d-89.3408056?entry=ttu" }
-    ],
-    b: [
-        { location: "xyz", lat: "", lng: "" },
-    ],
-    c: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    d: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    e: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    f: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    g: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    h: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    i: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    j: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    k: [
-        { location: "Sirmilik National Park, Canada", lat: "72°05'01.1 N", lng: "76°48'42.9 W", link: "https://www.google.com/maps/place/72%C2%B005'01.1%22N+76%C2%B048'42.9%22W/@72.1091739,-77.3683173,70440m/data=!3m1!1e3!4m12!1m7!3m6!1s0x4e17b70e5a3af4b1:0x1e43c34c0c4309a2!2sSirmilik+National+Park!8m2!3d73.3652488!4d-79.0152625!16zL20vMDZmeDZk!3m3!8m2!3d72.083628!4d-76.811917?entry=ttu" },
-        { location: "Golmud, China", lat: "35°36'46.3 N ", lng: "95°03'45.9 E", link: "https://maps.app.goo.gl/UweKoxYbPbz1xbY38" }
-    ],
-    l: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    m: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    n: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    o: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    p: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    q: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    r: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    s: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    t: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    u: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    v: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    w: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    x: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    y: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
-    z: [
-        { location: "xyz", lat: "", lng: "" },
-        { location: "xyz", lat: "", lng: "" }
-    ],
+export const letterData: Record<
+  string,
+  { location: string; lat: string; lng: string; link?: string }[]
+> = {
+  a: [
+    {
+      location: "Hickman, Kentucky",
+      lat: "36°35'20.8 N",
+      lng: "89°20'26.9 W",
+      link: "https://www.google.com/maps/place/36%C2%B035'20.8%22N+89%C2%B020'26.9%22W/@36.5689533,-89.4212718,11.38z/data=!4m4!3m3!8m2!3d36.5891111!4d-89.3408056?entry=ttu",
+    },
+    {
+      location: "Farm Island, Maine",
+      lat: "45°43'43.8 N ",
+      lng: "69°46'08.9 W",
+      link: "https://www.google.com/maps/place/45%C2%B043'45.4%22N+69%C2%B046'04.7%22W/@45.7292113,-69.7763592,6123m/data=!3m1!1e3!4m4!3m3!8m2!3d45.729275!4d-69.76798?entry=ttu",
+    },
+    {
+      location: "Lake Guakhmaz, Azerbaijan",
+      lat: "40°39'50.8 N",
+      lng: "47°06'36.2 E",
+      link: "https://maps.app.goo.gl/tnrVaiMuFiGacH746",
+    },
+    {
+      location: "Yukon Delta, Alaska",
+      lat: "62°33'17.7 N",
+      lng: "164°56'10.3 W",
+      link: "https://maps.app.goo.gl/AuARuTMTcT2ugLnq5",
+    },
+    {
+      location: "Lake Mjøsa, Norway",
+      lat: "60°45'52.7 N",
+      lng: "53°47'52.8 E",
+      link: "https://www.google.com/maps/place/36%C2%B035'20.8%22N+89%C2%B020'26.9%22W/@36.5689533,-89.4212718,11.38z/data=!4m4!3m3!8m2!3d36.5891111!4d-89.3408056?entry=ttu",
+    },
+  ],
+  b: [
+    {
+      location: "Holla Bend, Arkansas",
+      lat: "35°08'41.1 N",
+      lng: "93°03'16.5 W",
+      link: "https://www.google.com/maps/place/35%C2%B008'41.1%22N+93%C2%B003'16.5%22W/@35.150256,-93.0998349,20284m/data=!3m1!1e3!4m4!3m3!8m2!3d35.14475!4d-93.0545833?entry=ttu",
+    },
+    {
+      location: "Humaitá, Brazil",
+      lat: "7°37'00.1 S",
+      lng: "62°55'17.0 W",
+      link: "https://maps.app.goo.gl/BpsMZRtaDWr6jb9W8",
+    },
+  ],
+  c: [
+    {
+      location: "Black Rock Desert, Nevada",
+      lat: "40°47'15.8 N 119°12'13.0 W",
+      lng: "",
+      link: "https://www.google.com/maps/place/40%C2%B047'15.8%22N+119%C2%B012'13.0%22W/@40.7765477,-119.2977163,26799m/data=!3m1!1e3!4m4!3m3!8m2!3d40.7877222!4d-119.2036111?entry=ttu",
+    },
+    {
+      location: "Deception Island, Antarctica",
+      lat: "62°57'22.3 S",
+      lng: "60°38'32.8 W",
+      link: "https://www.google.com/maps/place/62%C2%B057'22.3%22S+60%C2%B038'32.8%22W/@-62.9597921,-60.8265853,38265m/data=!3m1!1e3!4m4!3m3!8m2!3d-62.9561944!4d-60.6424444?entry=ttu",
+    },
 
-}
+    {
+      location: "False River, Louisiana",
+      lat: "30°38'39.7 N",
+      lng: "91°26'45.7 W",
+      link: "https://www.google.com/maps/place/30%C2%B038'39.7%22N+91%C2%B026'45.7%22W/@30.6517944,-91.5063747,22066m/data=!3m1!1e3!4m4!3m3!8m2!3d30.644357!4d-91.446029?entry=ttu",
+    },
+  ],
+  d: [
+    {
+      location: "Akimiski Island, Canada",
+      lat: "53°00'58.5 N",
+      lng: "81°18'24.6 W",
+      link: "https://www.google.com/maps/place/53%C2%B000'58.5%22N+81%C2%B018'24.6%22W/@52.8535127,-81.6813828,147353m/data=!3m1!1e3!4m4!3m3!8m2!3d53.01625!4d-81.3068333?entry=ttu",
+    },
+    {
+      location: "Lake Tandou, Australia",
+      lat: "32°37'17.8 S",
+      lng: "142°04'21.4 E",
+      link: "https://maps.app.goo.gl/z3kPn1HxZUxS4FMCA",
+    },
+  ],
+  e: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  f: [
+    {
+      location: "Mato Grosso, Brazil",
+      lat: "13°50'26.9 S",
+      lng: " 55°17'55.0 W",
+      link: "https://maps.app.goo.gl/tXas4c27VuWi2pwk6",
+    },
+    {
+      location: "Kruger National Park, South Africa",
+      lat: "28°44'01.3 S",
+      lng: "29°12'30.1 E",
+      link: "https://maps.app.goo.gl/a1UArynvAqGGYMXg6",
+    },
+  ],
+  g: [
+    {
+      location: "Fonte Boa, Amazonas",
+      lat: "2°26'30.8 S",
+      lng: " 66°16'43.7 W",
+      link: "https://www.google.com/maps/place/2%C2%B026'30.8%22S+66%C2%B016'43.7%22W/@-2.4639488,-66.4453563,11z/data=!4m4!3m3!8m2!3d-2.4419!4d-66.2788?entry=ttu",
+    },
+  ],
+  h: [
+    {
+      location: "Southwestern Kyrgyzstan",
+      lat: "40°14'03.6 N",
+      lng: "71°14'22.8 E",
+      link: "https://maps.app.goo.gl/gRTJ6rDiiXJhT1mD9",
+    },
+    {
+      location: "Khorinsky District, Russia",
+      lat: "52°02'50.4 N",
+      lng: "109°46'51.2 E",
+      link: "https://maps.app.goo.gl/TLEGWnEGY8P3HUri9",
+    },
+  ],
+  i: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  j: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  k: [
+    {
+      location: "Sirmilik National Park, Canada",
+      lat: "72°05'01.1 N",
+      lng: "76°48'42.9 W",
+      link: "https://www.google.com/maps/place/72%C2%B005'01.1%22N+76%C2%B048'42.9%22W/@72.1091739,-77.3683173,70440m/data=!3m1!1e3!4m12!1m7!3m6!1s0x4e17b70e5a3af4b1:0x1e43c34c0c4309a2!2sSirmilik+National+Park!8m2!3d73.3652488!4d-79.0152625!16zL20vMDZmeDZk!3m3!8m2!3d72.083628!4d-76.811917?entry=ttu",
+    },
+    {
+      location: "Golmud, China",
+      lat: "35°36'46.3 N ",
+      lng: "95°03'45.9 E",
+      link: "https://maps.app.goo.gl/UweKoxYbPbz1xbY38",
+    },
+  ],
+  l: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  m: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  n: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  o: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  p: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  q: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  r: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  s: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  t: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  u: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  v: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  w: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  x: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  y: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+  z: [
+    { location: "xyz", lat: "", lng: "" },
+    { location: "xyz", lat: "", lng: "" },
+  ],
+};
